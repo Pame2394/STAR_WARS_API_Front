b36c73d09235f6886ba2093b13fdb397c3da610e
Pamela Contreras <pame2394@gmail.com>
2021-04-24 02:48:42 +0000

    las vistas funcionan

--- src/js/layout.js
+++ src/js/layout.js
@@ -28,6 +28,12 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
+						<Route exact path="/register">
+							<Register />
+						</Route>
+						<Route exact path="/login">
+							<Login />
+						</Route>
 						<Route exact path="/demo">
 							<Demo />
 						</Route>
